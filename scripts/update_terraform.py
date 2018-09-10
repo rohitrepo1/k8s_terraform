e318f929379efe805dd31e75a027517996334f66
Akhila-A <akhila.a.acharya@gmail.com>
2018-09-10 18:29:42 +0530

    Update update_terraform.py

--- scripts/update_terraform.py
+++ scripts/update_terraform.py
@@ -9,23 +9,19 @@ def main(clientname, region, nodecount, toolslist):
         directory = os.getcwd()
         print(directory)
         tools = toolslist.replace('[', ' ').replace(']', ' ').replace(',', ' ').split()
-        #strB = sys.argv[2].replace('[', ' ').replace(']', ' ').replace(',', ' ').split()
         tool = [i for i in tools]
-        #print tool
         print len(tool)
-        modified_list = " ".join(str(x) for x in tool)
-
+       if len(tool) > 0:
+          modified_list = "-".join(str(x) for x in tool)
+        else:
+          modified_list = ""
+        command = 'ssh-keygen -f '+clientname+' -q -P ""'
         print (modified_list , clientname , region , nodecount)
         modified_dict = {}
-        if modified_list == "" :
-                modified_dict = {'###SSH_KEY_NAME###': clientname, '###CLIENT###': clientname, '###IP_RANGE###': "11.1",
-                                 '###AWS_LOCATION###': region, '###NODE_COUNT###': nodecount,
-                                 '###TOOLS###':  modified_list }
-        else:
+        modified_dict = {'###SSH_KEY_NAME###': clientname, '###CLIENT###': clientname, '###IP_RANGE###': "11.1",
+                         '###AWS_LOCATION###': region, '###NODE_COUNT###': nodecount,
+                         '###TOOLS###': modified_list}
 
-                modified_dict = {'###SSH_KEY_NAME###' : clientname,'###CLIENT###' : clientname,'###IP_RANGE###' : "11.1",'###AWS_LOCATION###' : region, '###NODE_COUNT###' : nodecount, '###TOOLS###' : "\"" + modified_list + "\""}
-
-        #git.Git(directory).clone("https://github.com/projectethan007/k8s_terraform.git")
         fp1 = open(directory+"/k8s_terraform/modified.tf", "w")
         fp2 = open(directory+"/k8s_terraform/common_variables.tf", 'r')
         data = fp2.read()
@@ -35,6 +31,8 @@ def main(clientname, region, nodecount, toolslist):
         fp1.write(data)
         fp1.close()
         os.rename(directory+"/k8s_terraform/modified.tf", directory+"/k8s_terraform/common_variables.tf")
+        os.chdir(directory+"/k8s_terraform/ssh_key")
+        os.system(command)
 
 if __name__ == "__main__":
     main(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
