f2bbc620241ff3beb31425a382ace4ceb151cc6f
Akhila-A <akhila.a.acharya@gmail.com>
2018-10-15 15:53:12 +0530

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -29,5 +29,5 @@ mv ./kubectl /usr/local/bin/kubectl
 curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops-linux-amd64
 mv kops-linux-amd64 /usr/local/bin/kops
-TOOLS1=`echo $TOOLS|sed 's/-/ /g'|sed -e "s/.*/\"&\"/"`
+TOOLS1=`echo $TOOLS|sed 's/\// /g'|sed -e "s/.*/\"&\"/"`
 printf "export KOPS_STATE_STORE=$KOPS_STATE_STORE\nexport ZONES=$AWS_REGION"a"\nexport VPC=$VPC\nexport node_count=$NODE_COUNT\nexport node_size=m4.large\nexport master_size=t2.medium\nexport api_loadbalancer_type=public\nexport topology=private\nexport dns=private\nexport dns_zone=$DNS_ZONE\nexport CLIENT=$CLIENT\nexport INITIAL_ADMIN_USER=ethanadmin\nexport TOOLS=$TOOLS1\nexport password=$INNERSOURCE\nexport node_type=$NODE_TYPE" > /home/ubuntu/vars.sh
